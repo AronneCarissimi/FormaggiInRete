14209687824b6ed3dcba99ffcaf4d68140182228
AronneCarissimi <101709237+AronneCarissimi@users.noreply.github.com>
2024-01-29 13:25:15 +0100

    Delete frontend directory

--- frontend/eliminaFormaggio.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "formaggi");
-$idFormaggio = $_GET['id'];
-$sql = ("DELETE FROM formaggio WHERE ID = $idFormaggio");
-$conn->query($sql);
-$conn->close();
-echo "";
