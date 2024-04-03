a2fa84cde19175f426b0d62da81eee4463058b2c
AronneCarissimi <101709237+AronneCarissimi@users.noreply.github.com>
2024-04-03 21:41:01 +0200

    Delete formaggi3.sql

--- formaggi3.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `caseificio` (`NOME`, `UTENTE_ID`) VALUES ('caseificio 1', '1');
-INSERT INTO `scaffale` (`NOME`, `CASEIFICIO_ID`) VALUES ('scaffale 1', '1');
-INSERT INTO `SENSORE` (`NOME`, `CASEIFICIO_ID`) VALUES ('sensore 1', '1');
