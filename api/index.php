2bcd1d5af921525d14934984f0f79cc4bb555db8
AronneCarissimi <101709237+AronneCarissimi@users.noreply.github.com>
2024-04-15 10:56:55 +0000

    ahead

--- api/index.php
+++ api/index.php
@@ -5,18 +5,30 @@ $uri = $_SERVER['REQUEST_URI'];
 // Split the URI into segments
 $segments = explode('/', $uri);
 
+
+do {
+    $first = array_shift($segments);
+} while ($first != 'api');
+
+for($i = 0; $i < count($segments); $i++) {
+    $segments[$i] = strtoupper($segments[$i]);
+}
+
+
+//print_r ($segments);
+
 $conn = new mysqli("localhost", "root", "", "FORMAGGI");
 
-// Check if the 4th segment exists
-if (isset ($segments[4]) && ($segments[4] != "")) {
-    if (isset ($segments[5]) && ($segments[5] != "")) {
-        if (isset ($segments[6]) && ($segments[6] != "")) {
-            $query = "SELECT * FROM $segments[6]  WHERE  $segments[4]_ID=$segments[5]";
+
+if (isset ($segments[0]) && ($segments[0] != "")) {
+    if (isset ($segments[1]) && ($segments[1] != "")) {
+        if (isset ($segments[2]) && ($segments[2] != "")) {
+            $query = "SELECT * FROM $segments[2]  WHERE  $segments[0]_ID=$segments[1]";
         } else {
-            $query = "SELECT * FROM $segments[4] WHERE  ID=$segments[5]";
+            $query = "SELECT * FROM $segments[0] WHERE  ID=$segments[1]";
         }
     } else {
-        $query = "SELECT * FROM $segments[4]";
+        $query = "SELECT * FROM $segments[0]";
     }
 
     if ($conn->connect_error) {
