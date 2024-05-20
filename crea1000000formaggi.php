2301909c6da19bc0737715a725ceb5c975f9b7dc
AronneCarissimi <101709237+AronneCarissimi@users.noreply.github.com>
2024-05-20 11:04:23 +0000

    j

--- crea1000000formaggi.php
+++ crea1000000formaggi.php
@@ -6,21 +6,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-for($i=0; $i<100000; $i++){
-
-
 
 $sensore_id = 1;
-$scaffale_id = 1;
+echo "Inizio inserimento";
+for($i=0; $i<100000; $i++){    
+$scaffale_id = random_int(1, 8);
 $tipo = random_int(1, 6);
-$nome = "formaggio".$i;
-
-
+$nome = "formaggio ".$i;
 
 // query di inserimento
 $sql = "INSERT INTO `FORMAGGIO` (`NOME`, `SCAFFALE_ID`, `TIPO_ID`, `SENSORE_ID`) VALUES ('$nome', '$scaffale_id', '$tipo', '$sensore_id');";
 $conn->query($sql);
-
-echo "Formaggio inserito: ".$nome."<br>";
 }
+echo "Inserimento completato";
 $conn->close();
